cccb7ee1627b3702d35391f622c41be19b966174
Kadimane <kadimanegloriah@gmail.com>
2023-03-22 01:36:59 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+// index.js
+
+function writeCards(names) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+  }
+  // index.js
+
+function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+  }
+  
+  
